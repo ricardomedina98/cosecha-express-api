8f6434b87167b61dcf89a64251686ad21a90b9bc
guerrero1010 <guerrero01h@gmail.com>
2019-07-10 17:07:48 -0500

    Add tables in DB

--- SQL/Cosechaexpress2.sql
+++ SQL/Cosechaexpress2.sql
@@ -3,7 +3,7 @@ create table usuarios(
   nombre_empleado varchar(50) not null,
   nombre_usuario varchar(50) not null,
   contrasena text NOT NULL,
-  role varchar(100) NOT NULL,
+  id_role int  not null,
   fecha_creacion datetime not null,
   creado_por varchar(30) not null,
   fecha_ultima_modificacion datetime,
@@ -11,7 +11,63 @@ create table usuarios(
   status varchar(1) NOT NULL DEFAULT 'A',
   CONSTRAINT id_usuario_pk primary key(id_usuario),
   CONSTRAINT nombre_usuario_un unique(nombre_usuario),
-  CONSTRAINT chk_status_usuarios CHECK(status in ('A', 'I'))
+  CONSTRAINT chk_status_usuarios CHECK(status in ('A', 'I')),
+  constraint fk_usuarios_roles foreign key (id_role) references roles(id_role)
+);
+
+create table roles_modulos_usuarios(
+id_roles_modulos_usuarios int auto_increment,
+id_role int,
+id_modulo int,
+constraint pk_id_roles_modulos_usuarios primary key(id_roles_modulos_usuarios),
+constraint fk_roles_modulos_usuarios foreign key(id_role) references roles(id_role),
+constraint fk_modulos_roles_usuarios foreign key(id_modulo) references modulos(id_modulo)
+);
+
+create table modulo_permisos_usuarios(
+id_modulo_permisos_usuarios int auto_increment,
+id_modulo int, 
+id_permiso int,
+constraint pk_id_modulo_permiso primary key(id_modulo_permisos_usuarios),
+constraint fk_modulo_permisos_modulos foreign key (id_modulo) references modulos(id_modulo),
+constraint fk_modulo_permisos_permisos foreign key (id_permiso) references permisos(id_permiso)
+);
+
+create table roles(
+id_role int auto_increment,
+nombre_role varchar(25),
+constraint pk_id_role primary key(id_role)
+);
+
+create table modulos(
+id_modulo int auto_increment,
+nombre_modulo varchar(45),
+constraint pk_id_modulo primary key (id_modulo)
+);
+
+create table role_modulos(
+id_role_modulos int auto_increment,
+id_role int,
+id_modulo int,
+constraint pk_id_role_modulos primary key(id_role_modulos),
+constraint fk_role_modulos_roles foreign key(id_role) references roles(id_role),
+constraint fk_role_modulos_modulos foreign key(id_modulo) references modulos(id_modulo)
+);
+
+create table permisos(
+id_permiso int auto_increment,
+nombre_permiso varchar(45),
+descripcion varchar(100), 
+constraint pk_id_permiso primary key (id_permiso)
+);
+
+create table modulo_permisos(
+id_modulo_permiso int auto_increment,
+id_modulo int, 
+id_permiso int,
+constraint pk_id_modulo_permiso primary key(id_modulo_permiso),
+constraint fk_modulo_permisos_modulos foreign key (id_modulo) references modulos(id_modulo),
+constraint fk_modulo_permisos_permisos foreign key (id_permiso) references permisos(id_permiso)
 );
 
 CREATE TABLE mediciones(
